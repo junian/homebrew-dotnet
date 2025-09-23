681d7dc502a8fb295e75c53370b27c8c506fcdd4
junian <3881052+junian@users.noreply.github.com>
2025-09-23 18:12:46 +0700

    Update dotnet-sdk version and SHA256 checksums

---
+++ Casks/dotnet-sdk@2.0.rb
@@ -0,0 +1,49 @@
+cask "dotnet-sdk@2.0" do
+  arch arm: "x64", intel: "x64"
+
+  version "2.1.202"
+  sha256   arm: "52031e7f6e493468e6a56a9172c847f02c9858bceebcdad85c95687ed98a930c",
+         intel: "52031e7f6e493468e6a56a9172c847f02c9858bceebcdad85c95687ed98a930c"
+
+  url "https://builds.dotnet.microsoft.com/dotnet/Sdk/#{version}/dotnet-sdk-#{version}-osx-#{arch}.pkg"
+  name ".NET SDK"
+  desc "Developer platform"
+  homepage "https://www.microsoft.com/net/core#macos"
+
+  # This identifies releases with the same major/minor version as the current
+  # cask version. New major/minor releases occur annually in November and the
+  # check will automatically update its behavior when the cask is updated.
+  livecheck do
+    url "https://builds.dotnet.microsoft.com/dotnet/release-metadata/#{version.major_minor}/releases.json"
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    strategy :json do |json, regex|
+      json["releases"]&.map do |release|
+        version = release.dig("sdk", "version")
+        next unless version&.match(regex)
+
+        version
+      end
+    end
+  end
+
+  depends_on macos: ">= :ventura"
+
+  pkg "dotnet-sdk-#{version}-osx-#{arch}.pkg"
+
+  uninstall pkgutil: [
+    "com.microsoft.dotnet.*#{version.major_minor}*#{arch}",
+  ]
+
+  zap pkgutil: [
+        "com.microsoft.dotnet.*",
+        "com.microsoft.netstandard.pack.targeting.*",
+      ],
+      delete:  [
+        "/etc/paths.d/dotnet",
+        "/etc/paths.d/dotnet-cli-tools",
+      ],
+      trash:   [
+        "~/.dotnet",
+        "~/.nuget",
+      ]
+end
